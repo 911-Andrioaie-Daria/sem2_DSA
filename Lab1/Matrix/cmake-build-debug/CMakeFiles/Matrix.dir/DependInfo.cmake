b1e0d9d436e51bbfe2b3ae46d1cc638709b199b2
911-Andrioaie-Daria <72091687+911-Andrioaie-Daria@users.noreply.github.com>
2021-09-20 20:07:51 +0300

    Add files via upload

---
+++ Lab1/Matrix/cmake-build-debug/CMakeFiles/Matrix.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/App.cpp" "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/cmake-build-debug/CMakeFiles/Matrix.dir/App.cpp.obj"
+  "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/ExtendedTest.cpp" "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/cmake-build-debug/CMakeFiles/Matrix.dir/ExtendedTest.cpp.obj"
+  "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/Matrix.cpp" "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/cmake-build-debug/CMakeFiles/Matrix.dir/Matrix.cpp.obj"
+  "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/ShortTest.cpp" "C:/Users/daria/Documents/study/facultate/anul 1/semestrul 2/SDA/Lab/Lab 1/Matrix/cmake-build-debug/CMakeFiles/Matrix.dir/ShortTest.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
